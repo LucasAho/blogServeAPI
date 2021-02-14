d2ea0ef35f8d57f75ae344173f0625949082b30b
Lucas Aho <aholucascode@gmail.com>
2021-02-14 15:40:03 -0700

    cors added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require('cors');
 require("dotenv").config();
 const PORT = process.env.PORT || 5002;
 
@@ -10,7 +11,7 @@ const routes = require("./routes/api_routes");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(cors({ origin: true }));
 //app.use(express.static("index"));
 
 
