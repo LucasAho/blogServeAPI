d3c12f011d7d7339bbda834d393c3b9f79670825
Lucas Aho <aholucascode@gmail.com>
2021-05-12 14:16:55 -0600

    adding user validation on word creation

--- controllers/wordController.js
+++ controllers/wordController.js
@@ -2,10 +2,12 @@ const db = require("../models");
 
 module.exports = {
     create: function (req, res) {
-        db.WordModel
-            .create(req.body)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
+        if (req.body.validate === "aholucascode@gmail.com") {
+            db.WordModel
+                .create(req.body.data)
+                .then(dbModel => res.json(dbModel))
+                .catch(err => res.status(422).json(err));
+        }
     },
     findAll: function (req, res) {
         db.WordModel
