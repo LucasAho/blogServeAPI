6e49b681eaa6114342448e1793d76f3761967dbf
Lucas Aho <aholucascode@gmail.com>
2021-05-22 17:38:10 -0600

    Added nodemailer routes

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -30,6 +30,44 @@ function api_routes(app) {
 
     app.get("/api/conlang/tukren/wordlist/:word", wordController.findOne);
     app.get("/api/conlang/tukren/wordlist", wordController.findAll);
+
+    //////////////////////
+    ////Email Route//////
+    //////////////////////
+    app.post("/api/contact", 
+        function (req, res) {
+            let nodemailer = require('nodemailer');
+            const transporter = nodemailer.createTransport({
+                port: 465,
+                host: "smtp.gmail.com",
+                auth: {
+                    user: 'creativespectrum99@gmail.com',
+                    pass: process.env.CONTACT_PASS,
+                },
+                secure: true,
+            });
+            
+            const mailData = {
+                from: 'creativespectrum99@gmail.com',
+                to: 'aholucascode@gmail.com',
+                subject: `Message From ${req.body.name}`,
+                text: req.body.message + " | Sent from: " + req.body.email,
+                html: `<div>${req.body.message}</div><p>Sent from:
+                    ${req.body.email}</p>`
+            }
+
+            transporter.sendMail(mailData, function (err, info) {
+                if(err) {
+                    console.log(err)
+                } else {
+                    console.log(info);
+                }
+            });
+
+            res.status(200);
+            return res.end();
+        }
+    );
 }
 
 module.exports = api_routes;
